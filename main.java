bafdd5d28feda1cb1b2218738c94ee4a8ee6aea8
mohanasubanandini-ux <mohanasubanandini@gmail.com>
2026-02-23 08:26:57 +0530

    main.java

---
+++ main.java
@@ -0,0 +1,13 @@
+class SampleProgram1 {
+    public static void main(String[] args) {
+        String name = "Sam";
+        int rollNumber = 1;
+        double percentMarks = 99.99;
+        char result = 'P';   // 'P' for Pass, 'F' for Fail
+
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Percent Marks: " + percentMarks);
+        System.out.println("Result: " + result);
+    }
+}
