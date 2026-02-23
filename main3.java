2d751a1c34586dff9f676ac344b7e0a113f47b62
mohanasubanandini-ux <mohanasubanandini@gmail.com>
2026-02-23 09:19:41 +0530

    main4.java

---
+++ main3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SquareSide {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double perimeter, side;
+
+        System.out.print("Enter the perimeter of the square: ");
+        perimeter = sc.nextDouble();
+
+        side = perimeter / 4;   // Perimeter = 4 × side
+
+        System.out.println("The length of the side is " + side +
+                           " whose perimeter is " + perimeter);
+
+        sc.close();
+    }
+}
