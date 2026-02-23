891dd2c8b86e37ce4c09be7547a9e105b21900b2
mohanasubanandini-ux <mohanasubanandini@gmail.com>
2026-02-23 09:02:47 +0530

    main4.java

---
+++ main4.java
@@ -0,0 +1,15 @@
+class SampleProgram {
+    public static void main(String[] args) {
+
+        double costPrice = 129;
+        double sellingPrice = 191;
+
+        double profit = sellingPrice - costPrice;
+        double profitPercentage = (profit / costPrice) * 100;
+
+        System.out.println("Cost Price: INR " + costPrice +
+                           "\nSelling Price: INR " + sellingPrice +
+                           "\nProfit: INR " + profit +
+                           "\nProfit Percentage: " + profitPercentage + "%");
+    }
+}
